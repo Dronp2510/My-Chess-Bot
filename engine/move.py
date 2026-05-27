1ad94a862dd6cbcdb5bdd9fe130c468e1044fe3f
Dronp2510 <dronpatel2510@gmail.com>
2026-05-27 18:14:03 +0530

    best move notation added
    added basic move notation such as e4e8 , h2g3 , etc. Helps debugging.

--- engine/move.py
+++ engine/move.py
@@ -28,6 +28,47 @@ class Move:
 
     cols_to_files = {v: k for k, v in files_to_cols.items()}
 
+    # For Redable Moves
+    files_to_cols = {
+        'a':0, 'b':1, 'c':2, 'd':3,
+        'e':4, 'f':5, 'g':6, 'h':7
+    }
+
+    cols_to_files = {v:k for k,v in files_to_cols.items()}
+
+    ranks_to_rows = {
+        '1':7, '2':6, '3':5, '4':4,
+        '5':3, '6':2, '7':1, '8':0
+    }
+
+    rows_to_ranks = {v:k for k,v in ranks_to_rows.items()}
+
+    def get_rank_file(self, row, col):
+        return self.cols_to_files[col] + self.rows_to_ranks[row]
+    
+    def get_chess_notation(self):
+
+        if self.is_castle_move:
+            if self.end_col == 6:
+                return "O-O"
+            return "O-O-O"
+
+        move_string = (
+            self.get_rank_file(self.start_row, self.start_col) +
+            self.get_rank_file(self.end_row, self.end_col)
+        )
+
+        if self.is_pawn_promotion:
+            move_string += "Q"
+
+        return move_string
+    
+    def __str__(self):
+        return self.get_chess_notation()
+        
+    # End
+
+
     def __init__(self, start_sq, end_sq, board):
 
         self.start_row = start_sq[0]
